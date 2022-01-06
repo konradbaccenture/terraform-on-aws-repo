d4c0e557f4fd49a71dc7a847742ab6870a4ad2f9
konradbaccenture <87768573+konradbaccenture@users.noreply.github.com>
2022-01-06 15:21:19 +0000

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -10,7 +10,15 @@ resource "aws_s3_bucket" "my-s3-bucket" {
     enabled = var.versioning
   }
   
-  kms_key_arn = "arn:aws:kms:us-east-1:859661026757:key/fbb39b62-bf60-4f1b-81eb-248e64a9e7d5"
+  
+    server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "arn:aws:kms:us-east-1:859661026757:key/fbb39b62-bf60-4f1b-81eb-248e64a9e7d5"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
   
   tags = var.tags
 }
