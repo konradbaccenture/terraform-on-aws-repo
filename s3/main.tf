abc45a50d1e8b0969c6ceb81b3222d89b272e860
konradbaccenture <87768573+konradbaccenture@users.noreply.github.com>
2022-01-06 13:47:29 +0000

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -2,30 +2,10 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_s3_bucket" "my-s3-bucket" {
-  bucket_prefix = var.bucket_prefix
-  acl = var.acl
-  
-   versioning {
-    enabled = var.versioning
-  }
-  
-  tags = var.tags
-}
+
 
 resource "aws_s3_bucket" "KMS_bucket" {
   bucket_prefix = "KMS_bucket"
-  acl = var.acl
-  
-   versioning {
-    enabled = var.versioning
-  }
-  
-  tags = var.tags
-}
-
-resource "aws_s3_bucket" "CMK_bucket" {
-  bucket_prefix = "CMK_bucket"
   acl = var.acl
   
    versioning {
