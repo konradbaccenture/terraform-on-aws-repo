a781f6a49bca34fec28d87c06d7d71ef2ec1bbd7
konradbaccenture <87768573+konradbaccenture@users.noreply.github.com>
2022-01-06 15:06:05 +0000

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -10,5 +10,7 @@ resource "aws_s3_bucket" "my-s3-bucket" {
     enabled = var.versioning
   }
   
+  kms_key_arn = "arn:aws:kms:us-east-1:859661026757:key/fbb39b62-bf60-4f1b-81eb-248e64a9e7d5"
+  
   tags = var.tags
 }
