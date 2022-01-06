7b9352e723b5644018f7fa30d5d0595176298f30
konradbaccenture <87768573+konradbaccenture@users.noreply.github.com>
2022-01-06 14:29:43 +0000

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_s3_bucket" "my-s3-bucket" {
+resource "aws_s3_bucket" "default-s3-bucket" {
   bucket_prefix = var.bucket_prefix
   acl = var.acl
   
