b26d89e4d29ac208a211305b40c43af4c8db5bab
Konrad Bachusz <konradbachusz@gmail.com>
2022-01-13 17:01:45 +0000

    unencrypted the bucket

--- s3/main.tf
+++ s3/main.tf
@@ -12,19 +12,19 @@ resource "aws_s3_bucket" "my-s3-bucket" {
   bucket_prefix = var.bucket_prefix
   acl = var.acl
   
-   versioning {
-    enabled = var.versioning
-  }
+  #  versioning {
+  #   enabled = var.versioning
+  # }
   
   
-    server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = "arn:aws:kms:us-east-1:073510484680:key/bf2c50ad-5ccb-44ef-be1d-9907bef76182"
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
+  #   server_side_encryption_configuration {
+  #   rule {
+  #     apply_server_side_encryption_by_default {
+  #       kms_master_key_id = "arn:aws:kms:us-east-1:073510484680:key/bf2c50ad-5ccb-44ef-be1d-9907bef76182"
+  #       sse_algorithm     = "aws:kms"
+  #     }
+  #   }
+  # }
   
-  tags = var.tags
+  # tags = var.tags
 }
